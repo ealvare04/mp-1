d28c26efd4270fbbb94ab303ad260b284bb1d1e4
Edison Alvarez <ealvare@bu.edu>
2026-02-06 22:34:40 -0500

    fixed padding bugs in css, changed calculator script name

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
-console.log('Happy developing ✨')
+// console.log('Happy developing ✨')
+
+function checkNegative(output) {
+
+    let out = output.innerHTML
+
+    console.log("CHECKING OUTPUT: " + out);
+
+    // if output is a number (aka not set to "" by clear())
+    if (!isNaN(out)) {
+        // parse the NON-EMPTY string as a number
+        out = Number(out);
+
+        if (out < 0){
+            output.style.color = "red";
+        } else {
+            output.style.color = "black";
+        }
+    }
+}
+
+function doAddition() {
+    let output = document.getElementById("output");
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+
+    let result = Number(first) + Number(second);
+
+    // set output
+    output.innerHTML = String(result);
+
+    checkNegative(output);
+}
+
+function doSubtraction(){
+    let output = document.getElementById("output");
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+
+    let result = Number(first) - Number(second);
+
+    // set output
+    output.innerHTML = String(result);
+
+    checkNegative(output);
+}
+
+function doMultiplication(){
+    let output = document.getElementById('output');
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+
+    let result = Number(first) * Number(second);
+    // set output
+    output.innerHTML = String(result);
+
+    checkNegative(output);
+}
+
+function doDivision() {
+    let output = document.getElementById("output");
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+
+    let result = Number(first) / Number(second);
+
+    // set output
+    output.innerHTML = String(result);
+
+    checkNegative(output);
+}
+
+function doPower() {
+    let output = document.getElementById("output");
+    let first = document.getElementById("first").value;
+    let second = document.getElementById("second").value;
+
+    let result = 1;
+
+    for (let i=0; i<second; i++){
+        result = result * first
+    }
+
+    // set output
+    output.innerHTML = String(result);
+
+    checkNegative(output);
+}
+
+function doClear() {
+    console.log("CLEARING")
+
+    let output = document.getElementById("output");
+    let first = document.getElementById("first");
+    let second = document.getElementById("second");
+
+    // reset values and output
+    output.innerHTML = "";
+    first.value="";
+    second.value="";
+
+    checkNegative(output);
+
+    output.style.color = "black";
+}
+
